dd17f16c6905086f60172a0f77a115db81a32d14
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-15 16:54:54 +0100

    ajout babel-loader

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module '@babel/core'\\nRequire stack:\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/babel-loader/lib/index.js\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/loader-runner/lib/loadLoader.js\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/loader-runner/lib/LoaderRunner.js\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/webpack/lib/NormalModuleFactory.js\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/webpack/lib/Compiler.js\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/webpack/lib/webpack.js\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/webpack/lib/index.js\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/webpack-cli/lib/webpack-cli.js\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/webpack-cli/lib/bootstrap.js\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/webpack-cli/bin/cli.js\\n- /Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/webpack/bin/webpack.js\\n babel-loader@9 requires Babel 7.12+ (the package '@babel/core'). If you'd like to use Babel 6.x ('babel-core'), you should install 'babel-loader@7'.\\n    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:933:15)\\n    at Function.Module._load (node:internal/modules/cjs/loader:778:27)\\n    at Module.require (node:internal/modules/cjs/loader:1005:19)\\n    at require (node:internal/modules/cjs/helpers:102:18)\\n    at Object.<anonymous> (/Users/lukasz/Desktop/dummy/phenixRTS_dummy/node_modules/babel-loader/lib/index.js:5:11)\\n    at Module._compile (node:internal/modules/cjs/loader:1103:14)\\n    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1157:10)\\n    at Module.load (node:internal/modules/cjs/loader:981:32)\\n    at Function.Module._load (node:internal/modules/cjs/loader:822:12)\\n    at Module.require (node:internal/modules/cjs/loader:1005:19)\");\n\n//# sourceURL=webpack://my-webpack-project/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
