035192d96ba91e7f4667b792bd4ded12f580871a
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-15 16:24:59 +0100

    passage commonJS vers es6 modules test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
+/* const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { myFunction } = require('./myModule'); */
+
+import { path } from 'path';
+import { HtmlWebpackPlugin } from 'html-webpack-plugin';
 
 const isProduction = process.env.NODE_ENV == 'production';
 
