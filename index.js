53d0b6c41ac0f9b87635ddea2a4ccd9f3c3b11f6
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-15 12:08:05 +0100

    ajout balise gettime + ajout balise script remote ressource

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const sdk = require('phenix-web-sdk');
+const sdk = require('https://dl.phenixrts.com/WebSDK/2022.0.latest/phenix-web-sdk.min.js');
 
 console.log("1 WWWWWWWW",sdk);
 
 console.log("2 WWWWWWWW",sdk);
+
