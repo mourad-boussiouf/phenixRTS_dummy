bab044a7bc411320a24b430191d6478e9b492f53
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-03-17 10:59:41 +0100

    ajout style loader du webpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 console.log("Hello World!!!");
 
 import * as sdk from 'phenix-web-sdk';
+import "./style.css";
 
 console.log("1 WWWWWWWW",sdk);
 
 console.log("1 WWWWWWWW",sdk);
 
 console.log("2 WWWWWWWW",sdk);
+
+function clickEvent(){
+    console.log("click")
+}
